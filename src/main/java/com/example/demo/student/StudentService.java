cb6116de442868563dd971052bfe6322d8ff1eed
Deep Debnath <dj.debnath2000@gmail.com>
2022-08-03 13:13:35 +0530

    added data to db

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +10,14 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1,
-                        "BLitz",
-                        "bliyz@gmail.com",
-                        LocalDate.of(2001, Month.AUGUST,6),
-                        19
-                )
-        );
+        return studentRepository.findAll();
     }
 }
